c7bd36fad599b74e9455c0fabb1297c6233e6644
nabil2sib <garenaff480123@gmail.com>
2025-09-30 11:02:00 +0700

    penambahan respon

--- app/Http/Controllers/QuestionController.php
+++ app/Http/Controllers/QuestionController.php
@@ -28,9 +28,16 @@ class QuestionController extends Controller
     public function store(Request $request)
     {
         //dd($request->all()) ;
-        $data['nama'] = $request->nama;
+       	$request->validate([
+		    'nama'  => 'required|max:10',
+		    'email' => ['required','email'],
+		    'pertanyaan' => 'required|max:300|min:8',
+        ],[
+            'nama.required' => 'Nama Tidak Boleh Kosong',
+            'email.email' => 'email tidak valid',
+        ]);
     }
-
+    
     /**
      * Display the specified resource.
      */
