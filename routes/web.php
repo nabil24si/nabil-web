1a6b726e3893ddc47ab6216370a904133ee1a649
nabil2sib <garenaff480123@gmail.com>
2025-09-30 10:15:27 +0700

    penambahan form pertanyaan

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\QuestionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home',[HomeController::class,'index']);
+
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
 
